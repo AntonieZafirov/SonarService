a5a2286188ec841870ecb11f8934615809e12ac6
azafirov <zafirovantonie@hotmail.com>
2016-10-31 12:43:29 +0100

    init

---
+++ src/main/java/com/en/SonarServiceApplication.java
@@ -0,0 +1,13 @@
+package com.en;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarServiceApplication.class, args);
+	}
+}
